e064f3b38e4804c9de0fe6fe6cc4e4c6a741f54e
programista1024 <blogprogramisty1024@gmail.com>
2024-02-29 23:02:33 +0100

    dodanie prostego kontrolera

---
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package pl.programista1024.springboot.todowebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Cześć! Jakie masz rzeczy do zrobienia?";
+	}
+	
+}
