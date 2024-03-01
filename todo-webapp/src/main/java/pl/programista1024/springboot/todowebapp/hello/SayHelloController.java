010eb5859915bb1d241cdfddb1e7a706e1fe7810
programista1024 <blogprogramisty1024@gmail.com>
2024-03-01 22:24:49 +0100

    wprowadzenie prostego kontrolera oraz widoku JSP

--- todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/hello/SayHelloController.java
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/hello/SayHelloController.java
@@ -12,5 +12,29 @@ public class SayHelloController {
 	public String sayHello() {
 		return "Cześć! Jakie masz rzeczy do zrobienia?";
 	}
-	
+
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<!DOCTYPE html>");
+		
+		sb.append("<html lang=\"pl\">																");
+		sb.append("<head>																			");
+		sb.append("   <meta charset=\"UTF-8\">														");
+		sb.append("   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">	");
+		sb.append("   <title>Menadżer Zadań</title>													");
+		sb.append("</head>																			");
+		sb.append("<body>																			");
+		sb.append("   <h1>Witaj w Menadżerze Zadań</h1>												");
+		sb.append("</body>																			");
+		sb.append("</html>																			");
+
+		return sb.toString();
+	}
+
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
 }
---
+++ todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+
+<html lang="pl-PL">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Menadżer Zadań</title>
+</head>
+<h1>Witaj w Menadżerze Zadań!</h1>
+</html>
