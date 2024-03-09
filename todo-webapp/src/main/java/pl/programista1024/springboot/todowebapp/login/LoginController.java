c9a5a9274170a4f08a39f96fe533409ce1234db8
programista1024 <blogprogramisty1024@gmail.com>
2024-03-09 23:24:46 +0100

    Rozbudowa widoku do logowania

--- todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/login/LoginController.java
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/login/LoginController.java
@@ -1,16 +1,13 @@
 package pl.programista1024.springboot.todowebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	public String gotoLoginPage() {
 		return "login";
 	}
 	
--- todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -9,8 +9,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Logowanie</title>
 </head>
-<h3>Witaj, ${name}</h3>
-<h1>Logowanie</h1>
-<p>nazwa użytkownika:</p>
-<p>hasło:</p>
+<body>
+	<h3>Witaj na stronie logowania!</h3>
+	<h1>Logowanie</h1>
+	<form method="post">
+		Nazwa użytkownika: <input type="text" name="name">
+		Hasło: <input type="password" name="password">
+		<input type="submit">
+	</form>
+</body>
 </html>
