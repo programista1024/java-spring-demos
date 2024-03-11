feb498517ef56dad25fa63a1cdabbcda5ffb78c0
programista1024 <blogprogramisty1024@gmail.com>
2024-03-11 20:42:48 +0100

    Wyświetlanie danych logowania na nowej stronie JSP /welcome przy użyciu Modelu. Rozbudowa kontrolera o nową metodę POST.

--- todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/login/LoginController.java
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/login/LoginController.java
@@ -1,14 +1,25 @@
 package pl.programista1024.springboot.todowebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("login")
+	@RequestMapping(value="login",method=RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
 	}
 	
+	@RequestMapping(value="login",method=RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name,
+			@RequestParam String password, ModelMap model) {
+		model.put("name", name);
+		model.put("password", password); // Tymczasowo przesyłamy również hasło, żeby sprawdzić działanie
+		
+		return "welcome";
+	}
 }
---
+++ todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+
+<html lang="pl-PL">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Zalogowany użytkownik</title>
+</head>
+<body>
+	<div>Witaj w systemie!</div>
+	<div>Zalogowany użytkownik: ${name}</div>
+	<div>Hasło użytkownika: ${password}</div>
+</body>
+</html>
