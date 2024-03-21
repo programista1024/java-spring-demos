89a650e7cdb8077c86d215cc2bc9ff62040c1ad4
programista1024 <blogprogramisty1024@gmail.com>
2024-03-21 21:35:58 +0100

    Rozszerzenie klasy TodoService o logikę wstawiania nowego wpisu do listy statycznej

--- todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/todo/TodoService.java
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/todo/TodoService.java
@@ -11,13 +11,20 @@ public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
 
+	private static int todosCount = 10;
+	
 	static {
-		todos.add(new Todo(10, "student", "podstawy c++", LocalDate.now().plusMonths(3), false));
-		todos.add(new Todo(11, "student", "programowanie obiektowe c++", LocalDate.now().plusMonths(6), false));
-		todos.add(new Todo(12, "student", "programowanie w Java Spring", LocalDate.now().plusMonths(5), false));
+		todos.add(new Todo(++todosCount, "student", "podstawy c++", LocalDate.now().plusMonths(3), false));
+		todos.add(new Todo(++todosCount, "student", "programowanie obiektowe c++", LocalDate.now().plusMonths(6), false));
+		todos.add(new Todo(++todosCount, "student", "programowanie w Java Spring", LocalDate.now().plusMonths(5), false));
 	}
 	
 	public List<Todo> findByUsername(String username) {
 		return todos;
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
 	}
 }
