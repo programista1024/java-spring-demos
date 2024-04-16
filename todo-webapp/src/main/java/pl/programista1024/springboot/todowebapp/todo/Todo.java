30a4fe6ff6ad9c89bdb4ed886068fbb87f51cca6
programista1024 <blogprogramisty1024@gmail.com>
2024-04-17 00:16:29 +0200

    Mapowanie klasy Todo na tabelę w bazie H2 poprzez adnotację JPA Entity. Wstępne wypełnienie tabeli przykładowymi danymi ze skryptu

--- todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/todo/Todo.java
+++ todo-webapp/src/main/java/pl/programista1024/springboot/todowebapp/todo/Todo.java
@@ -2,11 +2,18 @@ package pl.programista1024.springboot.todowebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 
+	@Id
+	@GeneratedValue
 	private int id;
+	
 	private String username;
 	
 	@Size(min=10, message="Co najmniej 10 znaków")
